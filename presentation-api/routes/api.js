991635c5568f1aeb80411ff57a8cf5a4ec1fa923
Mike Cardillo <mike@mjcardillo.com>
2019-02-27 22:15:12 -0800

    Return /api/presentation results sorted alphabetically

--- presentation-api/routes/api.js
+++ presentation-api/routes/api.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var router = express.Router();
 const Presentation = require('../models/presentation')
+const sequelize = require('sequelize')
 
 /* GET home page. */
 router.get('/presentations', function(req, res, next) {
   Presentation.findAll({
-    attributes: ["id", "title", "presenter"]
+    attributes: ["id", "title", "presenter"],
+    order: [[sequelize.fn('lower', sequelize.col('title')), 'ASC']]
   })
   .then((rows) => {
     res.send(rows)
